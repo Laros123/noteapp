ef8307394750446b7ab8dc76f0a13253c7c06034
Laros123 <you@example.com>
2024-03-30 16:02:04 +0200

    Add views and templates for views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from notes.models import Note
 
 # Create your views here.
+
+class NoteListView(ListView):
+    model = Note
+    template_name = 'notes/note_list.html'
+
+class NoteDetailView(DetailView):
+    model = Note
+    template_name = 'notes/note_detail.html'
