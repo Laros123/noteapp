e0e960a8f1941d4f24aa60d30beb5b0aa93582cf
Laros123 <you@example.com>
2024-04-04 21:38:07 +0300

    Final(create add form for note)

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from notes.views import NoteListView, NoteDetailView
+from notes.views import NoteListView, NoteDetailView, add_note
 
 urlpatterns = [
-    path('', NoteListView.as_view(), name='note_list'),
-    path('<int:pk>/', NoteDetailView.as_view(), name='note_detail'),
+    path('', NoteListView.as_view(), name='note-list'),
+    path('<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
+    path('add-note/', add_note, name='add-note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from notes.models import Note
+from .forms import NoteForm
 
 # Create your views here.
 
 class NoteListView(ListView):
     model = Note
+    context_object_name = 'notes'
     template_name = 'notes/note_list.html'
 
 class NoteDetailView(DetailView):
     model = Note
+    context_object_name = 'note'
     template_name = 'notes/note_detail.html'
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note-list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/add_note.html', {'form': form})
